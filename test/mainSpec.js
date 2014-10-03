b1158e5f2613cb4e5cef3f2f319486e12615df16
Ned Ludd <ndldd1811@gmail.com>
2014-10-03 13:08:45 +0200

    karma config

---
+++ test/mainSpec.js
@@ -0,0 +1,15 @@
+
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+
+    expect(true).toBe(true);
+
+  });
+    it("contains spec with an expectation and more", function() {
+
+    expect(true).toBe(true);
+
+  });
+});
+
